dcdade46986dade92cc92f7fb9da4253fa0fad17
Nurnabi <nurnabi2023@excelitai.com>
2023-06-22 15:40:27 +0600

    added ---  image_picker add

--- lib/controller/home_screen_controller.dart
+++ lib/controller/home_screen_controller.dart
@@ -1,6 +1,26 @@
 import 'package:get/get.dart';
+import 'package:image_and_video_editing/utils/app_color_resources.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
-class HomeScreenController extends GetxController{
 
 
+class HomeScreenController extends GetxController {
+  Rx<File?> selectedImage = Rx<File?>(null);
+
+  Future<void> pickImageFromGallery() async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      selectedImage.value = File(pickedFile.path);
+    } else {
+      Get.snackbar(
+        'Alert',
+        'No image selected',
+        backgroundColor: AppColorResources.buttonColorBlue,
+        snackPosition: SnackPosition.BOTTOM,
+      );
+    }
+  }
 }
--- lib/view/screen/home_screen.dart
+++ lib/view/screen/home_screen.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:image_and_video_editing/controller/home_screen_controller.dart';
 
-import 'bottom_navbar_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String routeName = '/home_screen';
-  HomeScreen({super.key});
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       body: Center(
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 50.0.w),
@@ -19,18 +20,29 @@ class HomeScreen extends StatelessWidget {
             width: double.infinity,
             decoration: BoxDecoration(
               color: Color(0xFFd3f3f1),
-
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(child: IconButton(onPressed: (){}, icon: Icon(Icons.add))),
-                Text('Add files',style: TextStyle(color: Colors.purple),)
-              ],
-            )
+            child: GetBuilder<HomeScreenController>(
+              init: HomeScreenController(),
+              builder: (controller) => Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    child: IconButton(
+                      onPressed: controller.pickImageFromGallery,
+                      icon: Icon(Icons.add),
+                    ),
+                  ),
+                  Text(
+                    'Add files',
+                    style: TextStyle(color: Colors.purple),
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
     );
   }
 }
+
