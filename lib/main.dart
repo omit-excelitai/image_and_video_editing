e21c6c0100d2ddb3a34cda17aba95ee2677df06a
Nurnabi <nurnabi2023@excelitai.com>
2023-07-04 09:15:43 +0600

    added ---  text editor

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:image_and_video_editing/view/screen/add_project_screen.dart';
 import 'package:image_and_video_editing/view/screen/edit_screen.dart';
 import 'package:image_and_video_editing/view/screen/bottom_navbar_screen.dart';
 import 'package:image_and_video_editing/view/screen/home_screen.dart';
+import 'package:image_and_video_editing/view/screen/test_video_edit.dart';
 import 'package:image_and_video_editing/view/screen/view_screen.dart';
 import 'di_container.dart' as di;
 
@@ -50,6 +51,99 @@ class MyApp extends StatelessWidget {
         });
   }
 }
+
+
+// import 'dart:io';
+// import 'package:flutter/material.dart';
+// import 'package:image_painter/image_painter.dart';
+// import 'package:path_provider/path_provider.dart';
+//
+// void main() => runApp(ExampleApp());
+//
+// class ExampleApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Image Painter Example',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//         visualDensity: VisualDensity.adaptivePlatformDensity,
+//       ),
+//       home: ImagePainterExample(),
+//     );
+//   }
+// }
+
+// class ImagePainterExample extends StatefulWidget {
+//   @override
+//   _ImagePainterExampleState createState() => _ImagePainterExampleState();
+// }
+//
+// class _ImagePainterExampleState extends State<ImagePainterExample> {
+//   final _imageKey = GlobalKey<ImagePainterState>();
+//   final _key = GlobalKey<ScaffoldState>();
+//
+//   void saveImage() async {
+//     final image = await _imageKey.currentState?.exportImage();
+//     final directory = (await getApplicationDocumentsDirectory()).path;
+//     await Directory('$directory/sample').create(recursive: true);
+//     final fullPath =
+//         '$directory/sample/${DateTime.now().millisecondsSinceEpoch}.png';
+//     final imgFile = File('$fullPath');
+//     imgFile.writeAsBytesSync(image!.toList()); // Convert Uint8List? to List<int>
+//     ScaffoldMessenger.of(context).showSnackBar(
+//       SnackBar(
+//         backgroundColor: Colors.grey[700],
+//         padding: const EdgeInsets.only(left: 10),
+//         content: Row(
+//           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//           children: [
+//             const Text(
+//               "Image Exported successfully.",
+//               style: TextStyle(color: Colors.white),
+//             ),
+//             TextButton(
+//               onPressed: () => Process.run('open', [fullPath]),
+//               child: Text(
+//                 "Open",
+//                 style: TextStyle(
+//                   color: Colors.blue[200],
+//                 ),
+//               ),
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       key: _key,
+//       appBar: AppBar(
+//         title: const Text("Image Painter Example"),
+//         actions: [
+//           IconButton(
+//             icon: const Icon(Icons.save_alt),
+//             onPressed: saveImage,
+//           )
+//         ],
+//       ),
+//       body:
+//       ImagePainter.asset(
+//         "assets/images/1.png",
+//         key: _imageKey,
+//         scalable: true,
+//         initialStrokeWidth: 2,
+//         //textDelegate: DutchTextDelegate(),
+//         initialColor: Colors.green,
+//         initialPaintMode: PaintMode.line,
+//       ),
+//     );
+//   }
+// }
+
 
 
 // import 'package:flutter/material.dart';
